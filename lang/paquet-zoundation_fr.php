b4ffa2939e8098da4a0ff47ec0284720c618c56d
Debondt Didier <p@henix.be>
2014-08-31 18:39:53 +0200

    Init git

---
+++ lang/paquet-zoundation_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Z
+	'zoundation_description' => 'Squelettes qui fait le lien entre Z-core et Foundation. Inspiré par <a href=\"http://www.spip-contrib.net/Zpip-dist-en-HTML5\" title=\"Zpip-dist HTML5\">Zpip-dist HTML5</a>.',
+	'zoundation_nom' => 'Zoundation',
+	'zoundation_slogan' => 'Z-core + foundation !',
+);
+
+?>
---
+++ zoundation_options.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Options du plugin Zoundationau chargement
+ *
+ * @plugin     Zoundation
+ * @copyright  2013
+ * @author     Phenix
+ * @licence    GNU/GPL
+ * @package    SPIP\Zoundation\Options
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+ $GLOBALS['z_blocs'] = array('content','colonne_1','colonne_2','head','head_js','header','footer');
