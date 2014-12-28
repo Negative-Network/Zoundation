1bdf5039646fe8ae8a87daf5de1747eb630635cf
Debondt Didier <p@henix.be>
2014-12-28 12:21:37 +0100

    Fonction d'autorisation pour tester si une personne est connectée

--- zoundation_autoriser.php
+++ zoundation_autoriser.php
@@ -9,17 +9,19 @@ function zoundation_autoriser(){}
 
 
 /**
- * Fonction d'autorisation pour tester si une personne est connectée ou non
- * On utilise ce système pour éviter de générer trop de cache sur les tests
- * simple.
- *
- * @param mixed $faire
- * @param mixed $type
- * @param mixed $id
- * @param mixed $qui
- * @param mixed $opt
- * @access public
- */
-function zoundation_connecter($faire, $type, $id, $qui, $opt) {
+* Fonction d'autorisation pour tester si une personne est connectée ou non
+* On utilise ce système pour éviter de générer trop de cache sur les tests
+* simple.
+*
+* Il suffit ensuite de faire [(#AUTORISER{connecter}|oui) ... ] oui [(#AUTORISER{connecter}|non) ... ]
+*
+* @param mixed $faire
+* @param mixed $type
+* @param mixed $id
+* @param mixed $qui
+* @param mixed $opt
+* @access public
+*/
+function zoundation_connecter_dist($faire, $type, $id, $qui, $opt) {
     return ($qui['id_auteur'] !== 0);
 }
